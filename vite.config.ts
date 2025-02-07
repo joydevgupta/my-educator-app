bb7921f0b6d7b24d2a2ad7c8b0b460b19963e00d
joydevgupta <joydevgupta@hotmail.com>
2025-02-07 22:48:32 +0530

    Revision iii for gh-page view

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// "command" can be 'serve' or 'build', and "mode" can be 'development' or 'production' by default
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/my-educator-app/' : '/',
+    // ...other config
+  };
 });
