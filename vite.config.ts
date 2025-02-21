f749949a81cc2f3c0619c97f0d6d656436e78e32
joydevgupta <joydevgupta@hotmail.com>
2025-02-21 17:33:40 +0530

    Removing Header -EDUFINITE-& Replace Hero section Welcome message

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react';
 // "command" can be 'serve' or 'build', "mode" can be 'development' or 'production'
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
     base: mode === 'production' ? '/my-educator-app/' : '/',
-    // ...other config if needed
+    build: {
+      outDir: 'docs', // This replaces dist with docs
+    },
+    plugins: [react()],
   };
 });
